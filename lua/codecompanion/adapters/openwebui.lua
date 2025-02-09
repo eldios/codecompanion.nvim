733ca9c39d48b3b4815a30eccb152c6e4572de48
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-09 04:56:03 +0100

    openwebui: update implementation

--- lua/codecompanion/adapters/openwebui.lua
+++ lua/codecompanion/adapters/openwebui.lua
@@ -1,80 +1,40 @@
-local config = require("codecompanion.config")
-local curl = require("plenary.curl")
-local log = require("codecompanion.utils.log")
-local utils = require("codecompanion.utils.adapters")
+---@diagnostic disable: unused-local
+local log = require("codecompanion.log")
+local utils = require("codecompanion.utils")
 
----Get a list of available Ollama models
----@params self CodeCompanion.Adapter
----@params opts? table
----@return table
-local function get_models(self, opts)
-  local adapter = require("codecompanion.adapters").resolve(self)
-  if not adapter then
-    log:error("Could not resolve Ollama adapter in the `get_models` function")
-    return {}
-  end
-
-  adapter:get_env_vars()
-  local url = adapter.env_replaced.url
-
-  local headers = {
-    ["content-type"] = "application/json",
-  }
-  if adapter.env_replaced.api_key then
-    headers["Authorization"] = "Bearer " .. adapter.env_replaced.api_key
-  end
-
-  local ok, response = pcall(function()
-    return curl.get(url .. "/v1/models", {
-      sync = true,
-      headers = headers,
-      insecure = config.adapters.opts.allow_insecure,
-      proxy = config.adapters.opts.proxy,
-    })
-  end)
-  if not ok then
-    log:error("Could not get the Ollama models from " .. url .. "/v1/models.\nError: %s", response)
-    return {}
-  end
-
-  local ok, json = pcall(vim.json.decode, response.body)
-  if not ok then
-    log:error("Could not parse the response from " .. url .. "/v1/models")
-    return {}
-  end
-
-  local models = {}
-  for _, model in ipairs(json.data) do
-    table.insert(models, model.id)
-  end
-
-  if opts and opts.last then
-    return models[1]
-  end
-  return models
-end
-
----@class Ollama.Adapter: CodeCompanion.Adapter
-  return {
-    name = "openwebui",
-    formatted_name = "OpenWebUI",
-    roles = {
-      llm = "assistant",
-      user = "user",
+return {
+  name = "OpenWebUI",
+  options = {
+    model = {
+      default = "mistralai/Mistral-7B-Instruct-v0.2",
+      prompt = "The model to use for the request",
     },
-    opts = {
-      stream = true,
+    temperature = {
+      default = 0.7,
+      prompt = "The temperature to use for sampling",
     },
-    features = {
-      text = true,
+    top_p = {
+      default = 0.9,
+      prompt = "The top_p value to use for sampling",
+    },
+    top_k = {
+      default = 50,
+      prompt = "The top_k value to use for sampling",
+    },
+    max_tokens = {
+      default = 256,
+      prompt = "The maximum number of tokens to generate",
       tokens = true,
       vision = false,
     },
-    url = "${url}/api/chat/completions",
-    env = {
-      url = "http://localhost:8080",
-      api_key = nil,
-    },
+  },
+  url = "${url}/{chat_url}",
+  env = {
+    url = "http://localhost:8080",
+    chat_url = "api/chat",
+    api_key = "",
+  },
+  handlers = {
     ---Set the parameters
     ---@param self CodeCompanion.Adapter
     ---@param params table
@@ -89,242 +49,83 @@ end
     ---@param messages table Format is: { { role = "user", content = "Your prompt here" } }
     ---@return table
     form_messages = function(self, messages)
-      messages = utils.merge_messages(messages)
-      return { messages = messages }
-    end,
-
-    ---Returns the number of tokens generated from the LLM
-    ---@param self CodeCompanion.Adapter
-    ---@param data table The data from the LLM
-    ---@return number|nil
-    tokens = function(self, data)
-      if data then
-        local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
-
-        if not ok then
-          return
-        end
-
-        if json.eval_count then
-          log:debug("Done! %s", json.eval_count)
-          return json.eval_count
-        end
+      local formatted_messages = {}
+      for _, message in ipairs(messages) do
+        table.insert(formatted_messages, {
+          role = message.role,
+          content = message.content,
+        })
       end
+      return { inputs = formatted_messages }
     end,
 
-    ---Output the data from the API ready for insertion into the chat buffer
+    ---Parses the output for a chat buffer
     ---@param self CodeCompanion.Adapter
-    ---@param data table The streamed JSON data from the API, also formatted by the format_data callback
-    ---@return table|nil
+    ---@param data string
+    ---@return string
     chat_output = function(self, data)
-      local output = {}
-
-      if data and data ~= "" then
-        local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
-
-        if not ok then
-          return { status = "error" }
-        end
-
-        local message = json.message
-
-        if message.content then
-          output.content = message.content
-          output.role = message.role or nil
-        end
-
-        return {
-          status = "success",
-          output = output,
-        }
+      local response = vim.json.decode(data)
+      if response and response.response then
+        return response.response
+      else
+        return "Error: Could not parse response"
       end
-
-      return nil
     end,
 
-    ---Output the data from the API ready for inlining into the current buffer
+    ---Parses the output for inline buffer
     ---@param self CodeCompanion.Adapter
-    ---@param data table The streamed JSON data from the API, also formatted by the format_data handler
-    ---@param context table Useful context about the buffer to inline to
-    ---@return table|nil
+    ---@param data string
+    ---@param context table
+    ---@return string
     inline_output = function(self, data, context)
-      if data and data ~= "" then
-        local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
-
-        if not ok then
-          log:error("Error malformed json: %s", json)
-          return
-        end
-
-        return json.message.content
+      local response = vim.json.decode(data)
+      if response and response.response then
+        return response.response
+      else
+        return "Error: Could not parse response"
       end
     end,
 
-    ---Function to run when the request has completed. Useful to catch errors
+    ---Parses the tokens from the response
     ---@param self CodeCompanion.Adapter
-    ---@param data table
-    ---@return nil
+    ---@param data string
+    ---@return integer
+    tokens = function(self, data)
+      local response = vim.json.decode(data)
+      return response.tokens
+    end,
+
+    ---@param self CodeCompanion.Adapter
+    ---@param data string
     on_exit = function(self, data)
-      if data.status >= 400 then
-        log:error("Error: %s", data.body)
-      end
+      -- Messaage on exit
     end,
   },
-  schema = {
-    model = {
-      order = 1,
-      mapping = "parameters",
-      type = "enum",
-      desc = "ID of the model to use.",
-      default = function(self)
-        return get_models(self, { last = true })
-      end,
-      choices = function(self)
-        return get_models(self)
-      end,
-    },
-    temperature = {
-      order = 2,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 0.8,
-      desc = "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.",
-      validate = function(n)
-        return n >= 0 and n <= 2, "Must be between 0 and 2"
-      end,
-    },
-    num_ctx = {
-      order = 3,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 2048,
-      desc = "The maximum number of tokens that the language model can consider at once. This determines the size of the input context window, allowing the model to take into account longer text passages for generating responses. Adjusting this value can affect the model's performance and memory usage.",
-      validate = function(n)
-        return n > 0, "Must be a positive number"
-      end,
-    },
-    mirostat = {
-      order = 4,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 0,
-      desc = "Enable Mirostat sampling for controlling perplexity. (default: 0, 0 = disabled, 1 = Mirostat, 2 = Mirostat 2.0)",
-      validate = function(n)
-        return n == 0 or n == 1 or n == 2, "Must be 0, 1, or 2"
-      end,
-    },
-    mirostat_eta = {
-      order = 5,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 0.1,
-      desc = "Influences how quickly the algorithm responds to feedback from the generated text. A lower learning rate will result in slower adjustments, while a higher learning rate will make the algorithm more responsive. (Default: 0.1)",
-      validate = function(n)
-        return n > 0, "Must be a positive number"
-      end,
-    },
-    mirostat_tau = {
-      order = 6,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 5.0,
-      desc = "Controls the balance between coherence and diversity of the output. A lower value will result in more focused and coherent text. (Default: 5.0)",
-      validate = function(n)
-        return n > 0, "Must be a positive number"
-      end,
-    },
-    repeat_last_n = {
-      order = 7,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 64,
-      desc = "Sets how far back for the model to look back to prevent repetition. (Default: 64, 0 = disabled, -1 = num_ctx)",
-      validate = function(n)
-        return n >= -1, "Must be -1 or greater"
-      end,
-    },
-    repeat_penalty = {
-      order = 8,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 1.1,
-      desc = "Sets how strongly to penalize repetitions. A higher value (e.g., 1.5) will penalize repetitions more strongly, while a lower value (e.g., 0.9) will be more lenient. (Default: 1.1)",
-      validate = function(n)
-        return n >= 0, "Must be a non-negative number"
-      end,
-    },
-    seed = {
-      order = 9,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 0,
-      desc = "Sets the random number seed to use for generation. Setting this to a specific number will make the model generate the same text for the same prompt. (Default: 0)",
-      validate = function(n)
-        return n >= 0, "Must be a non-negative number"
-      end,
-    },
-    stop = {
-      order = 10,
-      mapping = "parameters.options",
-      type = "string",
-      optional = true,
-      default = nil,
-      desc = "Sets the stop sequences to use. When this pattern is encountered the LLM will stop generating text and return. Multiple stop patterns may be set by specifying multiple separate stop parameters in a modelfile.",
-      validate = function(s)
-        return s:len() > 0, "Cannot be an empty string"
-      end,
-    },
-    tfs_z = {
-      order = 11,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 1.0,
-      desc = "Tail free sampling is used to reduce the impact of less probable tokens from the output. A higher value (e.g., 2.0) will reduce the impact more, while a value of 1.0 disables this setting. (default: 1)",
-      validate = function(n)
-        return n >= 0, "Must be a non-negative number"
-      end,
-    },
-    num_predict = {
-      order = 12,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = -1,
-      desc = "Maximum number of tokens to predict when generating text. (Default: -1, -1 = infinite generation, -2 = fill context)",
-      validate = function(n)
-        return n >= -2, "Must be -2 or greater"
-      end,
-    },
-    top_k = {
-      order = 13,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 40,
-      desc = "Reduces the probability of generating nonsense. A higher value (e.g. 100) will give more diverse answers, while a lower value (e.g. 10) will be more conservative. (Default: 40)",
-      validate = function(n)
-        return n >= 0, "Must be a non-negative number"
-      end,
-    },
-    top_p = {
-      order = 14,
-      mapping = "parameters.options",
-      type = "number",
-      optional = true,
-      default = 0.9,
-      desc = "Works together with top-k. A higher value (e.g., 0.95) will lead to more diverse text, while a lower value (e.g., 0.5) will generate more focused and conservative text. (Default: 0.9)",
-      validate = function(n)
-        return n >= 0 and n <= 1, "Must be between 0 and 1"
-      end,
-    },
-  },
+  ---@param response table
+  ---@return string|nil
+  check_error = function(response)
+    if response.status_code ~= 200 then
+      local ok, json = pcall(vim.json.decode, response.body)
+      if not ok then
+        return "Failed to decode JSON: " .. response.body
+      end
+
+      if json and json.error then
+        return "Error: " .. json.error
+      else
+        return "Request failed with status code: " .. response.status_code .. " and body: " .. response.body
+      end
+    end
+    return nil
+  end,
+
+  ---@param opts table
+  ---@return table
+  get_headers = function(opts)
+    local headers = {}
+    if opts.env.api_key and opts.env.api_key ~= "" then
+      headers["Authorization"] = "Bearer " .. opts.env.api_key
+    end
+    return headers
+  end,
 }
